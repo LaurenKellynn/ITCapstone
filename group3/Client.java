8e699c24e73a081ce33f75d3b1e609d55b02a416
EmmaR120 <124412465+EmmaR120@users.noreply.github.com>
2023-02-18 00:13:49 -0500

    Add files via upload

---
+++ group3/Client.java
@@ -0,0 +1,36 @@
+/**
+ * Programmer name: Emma Rawstron
+ * Date: 2/6/2022
+ * IT Capstone Project: Query Application
+ * 
+ * The program will display a GUI with two query options:
+ * 		1. Display information for all customers
+ * 		2. Display order history for one customer
+ * 
+ * A table with query results and a print button will be displayed
+ */
+
+package group3;
+
+import javax.swing.SwingUtilities;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                constructGUI();
+            }
+        });
+
+    }
+
+    private static void constructGUI() {
+        SearchFrame frame = new SearchFrame();
+        //JDialog.setDefaultLookAndFeelDecorated(true)
+        //frame.pack();
+        frame.setVisible(true);
+    }
+
+}
---
+++ group3/PrintButtonListener.java
@@ -0,0 +1,55 @@
+/**
+ * Programmer name: Emma Rawstron
+ * Date: 2/6/2022
+ * IT Capstone Project: Query Application
+ */
+
+package group3;
+
+import java.awt.event.*;
+import java.awt.print.PrinterException;
+import java.text.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.JTable.PrintMode;
+
+public class PrintButtonListener implements ActionListener {
+    ResultPanel resultPanel;
+
+    public PrintButtonListener(ResultPanel inputResultPanel) {
+        resultPanel = inputResultPanel;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Locale locale = new Locale("en", "US");
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, locale);
+        String date = dateFormat.format(new Date());
+        //System.out.println(date);
+        MessageFormat header = new MessageFormat("Query Results:  ");
+        MessageFormat footer = new MessageFormat("Page {0}");
+        PrintMode printMode = JTable.PrintMode.NORMAL;
+
+        //Change print mode to fit width of page if check box is selected
+        if (resultPanel.printModeCheckBox.isSelected()) {
+            System.out.println(printMode.toString());
+            printMode = JTable.PrintMode.FIT_WIDTH;
+            System.out.println(printMode.toString());
+        }
+
+        //Change header to include the date if check box is selected
+        if (resultPanel.dateCheckBox.isSelected()) {
+            header = new MessageFormat("Query Results:  " + date);
+            System.out.println("Header: Query Results:  " + date);
+        }
+
+        try {
+            resultPanel.resultTable.print(printMode, header, footer); //Print the table
+        }
+        catch (PrinterException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
+
