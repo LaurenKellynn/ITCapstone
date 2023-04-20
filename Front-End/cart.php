4ad31dcbb059c161a82e4aaded3faf63bd57fbe6
EmmaR120 <124412465+EmmaR120@users.noreply.github.com>
2023-04-19 23:22:25 -0400

    Update cart.php

--- Front-End/cart.php
+++ Front-End/cart.php
@@ -18,7 +18,7 @@ $order_id = $_SESSION['order_id'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $con = new mysqli("itcapstonegroup3.mysql.database.azure.com", "ITCapstone3", "Group3isthebest!", "db_group3", 3306);
+    $con = new mysqli("itcapstonegroup3.mysql.database.azure.com", "ITCapstone3", "", "db_group3", 3306);
     if ($con->connect_error) {
         die('Connection Failed : '.$con->connect_error);
     } else {
